fd11bb184bbeb07c08f8bb08923c4a5e412562cc
konifar <yahpeycoy0403@gmail.com>
2015-08-01 18:18:30 +0900

    Created project

---
+++ example/src/androidTest/java/com/konifar/example/fabtransformation/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.konifar.example.fabtransformation;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ example/src/main/java/com/konifar/example/fabtransformation/MainActivity.java
@@ -0,0 +1,32 @@
+package com.konifar.example.fabtransformation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
